7cd51f53da6d480c0ba6948f93a81c77ae636e62
marioxd99 <marioxd99@gmail.com>
2020-10-12 20:06:37 +0200

    Segundo Cambio

--- src/Inico.java
+++ src/Inico.java
@@ -1,8 +1,8 @@
 
 public class Inico {
 	
-  public static void main(String[]args) {
-	  
+  public static void main(String[]args) {	  
 	  System.out.println("Holaa");
+	  System.out.print("Hey");
   }
 }
